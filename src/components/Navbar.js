bfd53cc9290b25ab725dbae3daaacb6a565860b2
Karthi <karthiraja268@gmail.com>
2021-10-19 19:31:31 +0530

    avatar added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import './Navbar.css';
+import React from "react";
+import "./Navbar.css";
+import IconButton from "@mui/material/IconButton";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import Avatar from "@mui/material/Avatar";
+import { deepOrange } from "@mui/material/colors";
 
 function Navbar() {
-    return (
-        <div className="container-fluid navbar-container">
-            <div className="nav-wrapper">
-                <div className="brand"><i className="fa fa-plane" aria-hidden="true"></i> Book<span className="brand-my">My</span>Trip</div>
-                <div className="profile">
-                    <span>Login/Register</span>
-                </div>
-            </div>
+  return (
+    <div className="container-fluid navbar-container">
+      <div className="nav-wrapper">
+        <div className="brand">
+          <i className="fa fa-plane" aria-hidden="true"></i> Book
+          <span className="brand-my">My</span>Trip
         </div>
-    )
+        <div className="profile">
+          {/* <span>
+            <IconButton aria-label="login" size="large">
+              <AccountCircleIcon fontSize="inherit" />
+            </IconButton>
+          </span> */}
+          <Avatar
+            src="/broken-image.jpg"
+            className="avatar"
+            sx={{ bgcolor: deepOrange["A200"],color:deepOrange[100] }}
+          />
+          <span className="login-register">Login/Register</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
