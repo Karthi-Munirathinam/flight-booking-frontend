8d33bcc45b6b2c851763236ae69c9d7ef658d03d
Karthi <karthiraja268@gmail.com>
2021-10-20 12:51:47 +0530

    added router to navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import './App.css';
-import Navbar from './components/Navbar';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Search from "./components/Search";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      {/* navbar -> company and login/register/forgot password*/}
-      {/* search flights */}
-      {/* search results */}
-      {/* booking */}
-      {/* payment */}
-      {/* footer */}
-      {/* flight seat layout */}
-      {/* ticket */}
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/'>
+            <Search />
+          </Route>
+        </Switch>
+        {/* navbar -> company and login/register/forgot password*/}
+        {/* search flights */}
+        {/* search results */}
+        {/* booking */}
+        {/* payment */}
+        {/* footer */}
+        {/* flight seat layout */}
+        {/* ticket */}
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,36 @@
 import React from "react";
 import "./Navbar.css";
-import IconButton from "@mui/material/IconButton";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+// import IconButton from "@mui/material/IconButton";
+// import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import Avatar from "@mui/material/Avatar";
 import { deepOrange } from "@mui/material/colors";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <div className="container-fluid navbar-container">
       <div className="nav-wrapper">
         <div className="brand">
-          <i className="fa fa-plane" aria-hidden="true"></i> Book
-          <span className="brand-my">My</span>Trip
+          <Link to='/' style={{ textDecoration: 'none', color: '#ff934f' }}>
+            <i className="fa fa-plane" aria-hidden="true"></i> Book
+            <span className="brand-my">My</span>Trip
+          </Link>
         </div>
-        <div className="profile">
-          {/* <span>
-            <IconButton aria-label="login" size="large">
-              <AccountCircleIcon fontSize="inherit" />
-            </IconButton>
-          </span> */}
-          <Avatar
-            src="/broken-image.jpg"
-            className="avatar"
-            sx={{ bgcolor: deepOrange["A200"],color:deepOrange[100] }}
-          />
-          <span className="login-register">Login/Register</span>
-        </div>
+        <Link to='/login' style={{ textDecoration: 'none', color: '#ff934f' }}>
+          <div className="profile">
+            {/* <span>
+              <IconButton aria-label="login" size="large">
+                <AccountCircleIcon fontSize="inherit" />
+              </IconButton>
+            </span> */}
+            <span className="avatar">
+              <Avatar
+                src="/broken-image.jpg"
+                sx={{ bgcolor: deepOrange["A200"], color: deepOrange[100] }}
+              /></span>
+            <span className="login-register">Login/Register</span>
+          </div>
+        </Link>
       </div>
     </div>
   );
