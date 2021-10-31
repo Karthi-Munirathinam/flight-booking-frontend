6b79ac087e13094247db9545fbedc04d8404b19a
Karthi <karthiraja268@gmail.com>
2021-10-31 17:53:44 +0530

    heroku link added

--- src/components/Connection.js
+++ src/components/Connection.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    // baseURL: "https://mk-url-shortener.herokuapp.com"
-    baseURL: "http://localhost:5000"
+    baseURL: "https://mkbookmytrip.herokuapp.com/"
+    // baseURL: "http://localhost:5000"
 })
 
 export default instance
